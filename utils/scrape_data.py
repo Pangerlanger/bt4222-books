cf16384e2c6f90f172e9648345c14818e0521d92
Zhou Leyang <lyzhou717@gmail.com>
2024-10-07 23:39:57 +0800

    Update scrape data script

--- utils/scrape_data.py
+++ utils/scrape_data.py
@@ -16,9 +16,31 @@ Instructions:
 """
 
 # Replace with your API key
-API_KEY = "AIzaSyCFw_Swc8HiLh7qri1PLVRGDjChNfsMNjw"
+API_KEY = [#"AIzaSyCFw_Swc8HiLh7qri1PLVRGDjChNfsMNjw",
+           #"AIzaSyAOxUnS3WzsM1tuZkDnlopaBHQyPWcwmAs",
+           #"AIzaSyCalbnS5jeGOZP2VIEwpBI0upryR3MDMqA",
+           #"AIzaSyABJauHLzk90FlhBrMeU7mxG-cIWAt_zNs",
+           #"AIzaSyB3xqviQy-Xvo1WVleUXixX2JLEvrWXF2Q",
+           "AIzaSyC4OKjNyeXy5an3YF9Pmkuyh_xEWETm3Og",
+           "AIzaSyBacJhtWccHveVOj3JLYAC1OcJRqNDroCo",
+           "AIzaSyDIq-XHe0hLgi3_pNwSLItO0vNHnVezutI",
+           "AIzaSyBFCnJuM7eELiDsjS-LsDJMSYdFImvB2B8",
+           "AIzaSyBPLH0uoFtMpPcDlIuaAVVmPdmxRU_FeYM",
+           "AIzaSyCVFrp9gOsvG125UhRkKqRK47zIFt7t9l8",
+           "AIzaSyApwmU4CEBnrde4fSGTsY5u72xFweXG_kg",
+           #"AIzaSyAevtLyR3LyO9iNGqqckpCxC5qPfy4dQJY",
+           #"AIzaSyBukwnIGd5XOv5scP_xqsxIAwoWqtJsJpw",
+           #"AIzaSyCsZMzc2w0l437ULnR5rWRYa2G9eh9PvRQ",
+           #"AIzaSyBjb9uV6UMHpZLfNLA2ylV9vBhR1d1jb7g",
+           #"AIzaSyDSSARKpZrrm5_q8JlnGv-clnf-2xpeQEM",
+           #"AIzaSyCk-EWPCS1advNKcBstHvl9mUBLGQrWYR8"
+           ]
+
+
+           
 # Replace with updated index
-START = 0
+START = 22000
+QUERYLIMIT = 1000
 
 column_mapping = {
     'language_code': 'language',
@@ -40,8 +62,8 @@ def check_missing_value(val) -> bool:
         return val == ''
     return val == None
 
-def update_missing_info(df: pd.DataFrame, row: pd.Series, row_idx: int, data: Dict) -> None:
-    """Updates DF with missing values"""
+def update_missing_info(df: pd.DataFrame, row: pd.Series, data: Dict) -> None:
+    """Updates row with missing values"""
 
     data['publishedDate'] = pd.to_datetime(data['publishedDate'])
 
@@ -57,46 +79,66 @@ def update_missing_info(df: pd.DataFrame, row: pd.Series, row_idx: int, data: Di
                 else:
                     new_val = data[column_mapping[col]]
 
-                df.at[row_idx, col] = new_val
+                row[col] = new_val
+    df.loc[len(df)] = row
         
 
 def scrape_data(df) -> None:
     counter = None
     query_count = 0
+    api_idx = 0
     categories = []
+    update_df = pd.DataFrame(columns=df.columns)
     error_df = pd.DataFrame(columns=df.columns)
 
     try:
-        while query_count <= 1000:
-            for idx, row in df.iterrows():
-                query_count += 1
-                counter = idx
-                isbn = row['isbn'] if row['isbn'] else row['isbn13']
+        for idx, row in df.iterrows():
+            key = API_KEY[api_idx]
+            if query_count >= 1000:
+                if api_idx == (len(API_KEY)-1):
+                    break
+                query_count = 0
+                api_idx += 1
+                key = API_KEY[api_idx]
+            query_count += 1
+            counter = idx
 
-                url = f"https://www.googleapis.com/books/v1/volumes?q=isbn:{isbn}&key={API_KEY}"
-                print(url)
+            if check_missing_value(row['isbn']):
+                if check_missing_value(row['isbn13']):
+                    isbn = None
+                else:
+                    isbn = row['isbn13']
+            else:
+                isbn = row['isbn']
+
+            if isbn:
+                url = f"https://www.googleapis.com/books/v1/volumes?q=isbn:{isbn}&key={key}"
+                print(f"{counter}: {url}")
                 try:
                     response = requests.get(url)
                     data = response.json()['items'][0]['volumeInfo']
-                    update_missing_info(df, row, idx, data)
+                    update_missing_info(update_df, row.copy(), data)
 
                     if 'categories' in data:
                         categories.append(data['categories'])
                     else:
                         categories.append([])
                 except:
-                    error_df.loc[len(df)] = row
-                    categories.append([])
+                    print("ERROR")
+                    error_df.loc[len(error_df)] = row
+            else:
+                error_df.loc[len(error_df)] = row
     
     except Exception as e:
         print(f"Unexpected error occurred: {e}")
-    
-    df['categories'] = categories
-    df.to_csv('missing_update_df.csv', index=False)
-    error_df.to_csv('error_df.csv', index=False)
+    print(categories)
+    print(update_df)
+    update_df['categories'] = categories
+    update_df.to_csv('update_df5.csv', index=False)
+    error_df.to_csv('error_df5.csv', index=False)
     print(f"Script stopped running at index: {counter}")
 
     
 if __name__ == "__main__":
-    df = pd.read_csv('missing_df.csv')[START:]
+    df = pd.read_csv('non_missing_df.csv')[START:]
     scrape_data(df)
